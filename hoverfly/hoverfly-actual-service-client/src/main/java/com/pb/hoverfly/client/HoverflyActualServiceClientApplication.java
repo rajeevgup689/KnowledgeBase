5ddd979a003bf8b4f5997977ccb97380c80d987a
Rajeev Gupta <rajeev.gup689@gmail.com>
2019-05-13 19:20:15 +0530

    hoverfly-actual-service-client

---
+++ hoverfly/hoverfly-actual-service-client/src/main/java/com/pb/hoverfly/client/HoverflyActualServiceClientApplication.java
@@ -0,0 +1,13 @@
+package com.pb.hoverfly.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoverflyActualServiceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoverflyActualServiceClientApplication.class, args);
+	}
+
+}
